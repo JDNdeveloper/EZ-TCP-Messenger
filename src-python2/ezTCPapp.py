38b176aa5cb9fca4045ac78082f6b4528c4dc8ba
Jayden Navarro <jdndeveloper@gmail.com>
2015-11-08 10:55:31 -0800

    Created Python 2 version of program.

---
+++ src-python2/ezTCPapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from ezTCPlib import TcpP2P
+
+name = raw_input( "Enter your first name: " )
+print
+ip = raw_input( "Enter target IP address: " )
+print
+cPort = int(raw_input( "Enter target port: " ))
+print
+sPort = int(raw_input( "Enter local port: " ))
+print
+
+if len(name) == 0: exit(1)
+
+user = name[0].upper()
+
+tcpP2P = TcpP2P( user, ip, cPort, sPort, 1024, 20 )
+
+tcpP2P.start()
---
+++ src-python3/ezTCPapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from ezTCPlib import TcpP2P
+
+name = input( "Enter your first name: " )
+print()
+ip = input( "Enter target IP address: " )
+print()
+cPort = int(input( "Enter target port: " ))
+print()
+sPort = int(input( "Enter local port: " ))
+print()
+
+if len(name) == 0: exit(1)
+
+user = name[0].upper()
+
+tcpP2P = TcpP2P( user, ip, cPort, sPort, 1024, 20 )
+
+tcpP2P.start()
